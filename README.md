d5d73b4ff16f7f91a86cc6c61f17bc5ebfa09acd
Narendra rawat <rawatnarendra009@gmail.com>
2024-01-17 15:22:01 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Update config.yaml
 Update params.yaml
 Update entity
 Update the configuration manager in src config
-update the conponents
+update the components
 update the pipeline
 update the main.py
 update the app.py
--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -11,7 +11,7 @@ from typing import Any
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
-            content= yaml.load(yaml_file,Loader=yaml.FullLoader)
+            content= yaml.safe_load(yaml_file)
             logger.info(f"yaml file:{path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
